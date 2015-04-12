9fd5bb749702fa36bac31543df2f9d8138e06873
Arktronic <arktronic@gmail.com>
2015-04-12 09:23:07 -0400

    Made Twitter not initialize when env vars are absent

--- twitter.rb
+++ twitter.rb
@@ -3,6 +3,10 @@ require 'json'
 require 'twitter'
 
 def twitter_init
+  if (ENV["TWITTER_CONSUMER_SECRET"].nil? || ENV["TWITTER_TOKEN_SECRET"].nil?)
+    return
+  end
+  
   $twitter_client = Twitter::Streaming::Client.new do |config|
     config.consumer_key        = "xHqPm3OaIJ9EVS3oIB0elQyQg"
     config.consumer_secret     = ENV.fetch("TWITTER_CONSUMER_SECRET")
@@ -26,6 +30,11 @@ end
 
 get '/tweets' do
   content_type :json
+  
+  if $twitter_mutex.nil?
+    halt({ :error => "Could not initialize Twitter stream" }.to_json)
+  end
+  
   $twitter_mutex.synchronize do
       @minTime = Time.at(params['after'].nil? ? 0 : params['after'].to_i)
       @tweets = $tweet_buffer.map { |t| { :text => t.text, :user_handle => t.user.screen_name, :user_name => t.user.name, :user_image => t.user.profile_image_url.to_s, :created => t.created_at, :timestamp => t.created_at.to_i } }
